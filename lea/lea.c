ce1f8304523dec4ea325cddbf2ae725948afed1b
ilwoong <ilwoong.jeong@gmail.com>
2018-06-21 22:23:20 +0900

    indentations were corrected

--- lea/lea.c
+++ lea/lea.c
@@ -63,23 +63,23 @@ static inline void lea_encrypt(uint8_t* out, const uint8_t* in, const uint8_t* r
     for (round = 0; round < rounds; round += 4)
     {
         b3 = ror32((b2 ^ rk[4]) + (b3 ^ rk[5]), 3);
-		b2 = ror32((b1 ^ rk[2]) + (b2 ^ rk[3]), 5);
-		b1 = rol32((b0 ^ rk[0]) + (b1 ^ rk[1]), 9);
+        b2 = ror32((b1 ^ rk[2]) + (b2 ^ rk[3]), 5);
+        b1 = rol32((b0 ^ rk[0]) + (b1 ^ rk[1]), 9);
         rk += 6;
 
-		b0 = ror32((b3 ^ rk[4]) + (b0 ^ rk[5]), 3);
-		b3 = ror32((b2 ^ rk[2]) + (b3 ^ rk[3]), 5);
-		b2 = rol32((b1 ^ rk[0]) + (b2 ^ rk[1]), 9);
+        b0 = ror32((b3 ^ rk[4]) + (b0 ^ rk[5]), 3);
+        b3 = ror32((b2 ^ rk[2]) + (b3 ^ rk[3]), 5);
+        b2 = rol32((b1 ^ rk[0]) + (b2 ^ rk[1]), 9);
         rk += 6;
 
         b1 = ror32((b0 ^ rk[4]) + (b1 ^ rk[5]), 3);
-		b0 = ror32((b3 ^ rk[2]) + (b0 ^ rk[3]), 5);
-		b3 = rol32((b2 ^ rk[0]) + (b3 ^ rk[1]), 9);
+        b0 = ror32((b3 ^ rk[2]) + (b0 ^ rk[3]), 5);
+        b3 = rol32((b2 ^ rk[0]) + (b3 ^ rk[1]), 9);
         rk += 6;
 
         b2 = ror32((b1 ^ rk[4]) + (b2 ^ rk[5]), 3);
-		b1 = ror32((b0 ^ rk[2]) + (b1 ^ rk[3]), 5);
-		b0 = rol32((b3 ^ rk[0]) + (b0 ^ rk[1]), 9);
+        b1 = ror32((b0 ^ rk[2]) + (b1 ^ rk[3]), 5);
+        b0 = rol32((b3 ^ rk[0]) + (b0 ^ rk[1]), 9);
         rk += 6;
     }
 
@@ -105,23 +105,23 @@ static inline void lea_decrypt(uint8_t* out, const uint8_t* in, const uint8_t* r
     for (round = 0; round < rounds; round += 4)
     {
         b0 = (ror32(b0, 9) - (b3 ^ rk[0])) ^ rk[1];
-		b1 = (rol32(b1, 5) - (b0 ^ rk[2])) ^ rk[3];
-		b2 = (rol32(b2, 3) - (b1 ^ rk[4])) ^ rk[5];
+        b1 = (rol32(b1, 5) - (b0 ^ rk[2])) ^ rk[3];
+        b2 = (rol32(b2, 3) - (b1 ^ rk[4])) ^ rk[5];
         rk -= 6;
 
         b3 = (ror32(b3, 9) - (b2 ^ rk[0])) ^ rk[1];
-		b0 = (rol32(b0, 5) - (b3 ^ rk[2])) ^ rk[3];
-		b1 = (rol32(b1, 3) - (b0 ^ rk[4])) ^ rk[5];
+        b0 = (rol32(b0, 5) - (b3 ^ rk[2])) ^ rk[3];
+        b1 = (rol32(b1, 3) - (b0 ^ rk[4])) ^ rk[5];
         rk -= 6;
 
         b2 = (ror32(b2, 9) - (b1 ^ rk[0])) ^ rk[1];
-		b3 = (rol32(b3, 5) - (b2 ^ rk[2])) ^ rk[3];
-		b0 = (rol32(b0, 3) - (b3 ^ rk[4])) ^ rk[5];
+        b3 = (rol32(b3, 5) - (b2 ^ rk[2])) ^ rk[3];
+        b0 = (rol32(b0, 3) - (b3 ^ rk[4])) ^ rk[5];
         rk -= 6;
 
         b1 = (ror32(b1, 9) - (b0 ^ rk[0])) ^ rk[1];
-		b2 = (rol32(b2, 5) - (b1 ^ rk[2])) ^ rk[3];
-		b3 = (rol32(b3, 3) - (b2 ^ rk[4])) ^ rk[5];
+        b2 = (rol32(b2, 5) - (b1 ^ rk[2])) ^ rk[3];
+        b3 = (rol32(b3, 3) - (b2 ^ rk[4])) ^ rk[5];
         rk -= 6;
     }
 
@@ -137,30 +137,30 @@ static inline void lea_decrypt(uint8_t* out, const uint8_t* in, const uint8_t* r
 void lea128_keygen(uint8_t* out, const uint8_t* mk)
 {
     uint32_t* rk = (uint32_t*) out;
-	uint32_t* t = (uint32_t*) mk;	
-	size_t round = 0;
-	
-	uint32_t t0 = t[0];
-	uint32_t t1 = t[1];
-	uint32_t t2 = t[2];
-	uint32_t t3 = t[3];
+    uint32_t* t = (uint32_t*) mk;    
+    size_t round = 0;
+    
+    uint32_t t0 = t[0];
+    uint32_t t1 = t[1];
+    uint32_t t2 = t[2];
+    uint32_t t3 = t[3];
 
-	for(round = 0; round < LEA128_ROUNDS; ++round) {
-		uint32_t delta = DELTA[round & 3];
-		
-		t0 = rol32(t0 + rol32(delta, round), 1);
-		t1 = rol32(t1 + rol32(delta, round + 1), 3);
-		t2 = rol32(t2 + rol32(delta, round + 2), 6);
-		t3 = rol32(t3 + rol32(delta, round + 3), 11);
+    for(round = 0; round < LEA128_ROUNDS; ++round) {
+        uint32_t delta = DELTA[round & 3];
+        
+        t0 = rol32(t0 + rol32(delta, round), 1);
+        t1 = rol32(t1 + rol32(delta, round + 1), 3);
+        t2 = rol32(t2 + rol32(delta, round + 2), 6);
+        t3 = rol32(t3 + rol32(delta, round + 3), 11);
 
-		rk[0] = t0;
+        rk[0] = t0;
         rk[1] = t1;
         rk[2] = t2;
         rk[3] = t1;
         rk[4] = t3;
         rk[5] = t1;
         rk += 6;
-	}
+    }
 }
 
 void lea128_encrypt(uint8_t* out, const uint8_t* in, const uint8_t* rks)
@@ -179,34 +179,34 @@ void lea128_decrypt(uint8_t* out, const uint8_t* in, const uint8_t* rks)
 void lea192_keygen(uint8_t* out, const uint8_t* mk)
 {
     uint32_t* rk = (uint32_t*) out;
-	uint32_t* t = (uint32_t*) mk;	
-	size_t round = 0;
-	
-	uint32_t t0 = t[0];
-	uint32_t t1 = t[1];
-	uint32_t t2 = t[2];
-	uint32_t t3 = t[3];
+    uint32_t* t = (uint32_t*) mk;    
+    size_t round = 0;
+    
+    uint32_t t0 = t[0];
+    uint32_t t1 = t[1];
+    uint32_t t2 = t[2];
+    uint32_t t3 = t[3];
     uint32_t t4 = t[4];
     uint32_t t5 = t[5];
 
-	for(round = 0; round < LEA192_ROUNDS; ++round) {
-		uint32_t delta = DELTA[round % 6];
-		
-		t0 = rol32(t0 + rol32(delta, round), 1);
-		t1 = rol32(t1 + rol32(delta, round + 1), 3);
-		t2 = rol32(t2 + rol32(delta, round + 2), 6);
-		t3 = rol32(t3 + rol32(delta, round + 3), 11);
+    for(round = 0; round < LEA192_ROUNDS; ++round) {
+        uint32_t delta = DELTA[round % 6];
+        
+        t0 = rol32(t0 + rol32(delta, round), 1);
+        t1 = rol32(t1 + rol32(delta, round + 1), 3);
+        t2 = rol32(t2 + rol32(delta, round + 2), 6);
+        t3 = rol32(t3 + rol32(delta, round + 3), 11);
         t4 = rol32(t4 + rol32(delta, round + 4), 13);
         t5 = rol32(t5 + rol32(delta, round + 5), 17);
 
-		rk[0] = t0;
+        rk[0] = t0;
         rk[1] = t1;
         rk[2] = t2;
         rk[3] = t3;
         rk[4] = t4;
         rk[5] = t5;
         rk += 6;
-	}
+    }
 }
 
 void lea192_encrypt(uint8_t* out, const uint8_t* in, const uint8_t* rks)
@@ -225,30 +225,30 @@ void lea192_decrypt(uint8_t* out, const uint8_t* in, const uint8_t* rks)
 void lea256_keygen(uint8_t* out, const uint8_t* mk)
 {
     uint32_t* rk = (uint32_t*) out;
-	uint32_t* keys = (uint32_t*) mk;
-	size_t round = 0;
+    uint32_t* keys = (uint32_t*) mk;
+    size_t round = 0;
 
     uint32_t t[8] = {0, };
     memcpy(t, keys, 8 * sizeof(uint32_t));
-	
-	for(round = 0; round < LEA256_ROUNDS; ++round) {
-		uint32_t delta = DELTA[round & 0x7];
-		
-		t[(6 * round) & 0x7] = rol32(t[(6 * round) & 0x7] + rol32(delta, round), 1);
-		t[(6 * round + 1) & 0x7] = rol32(t[(6 * round + 1) & 0x7] + rol32(delta, round + 1), 3);
-		t[(6 * round + 2) & 0x7] = rol32(t[(6 * round + 2) & 0x7] + rol32(delta, round + 2), 6);
-		t[(6 * round + 3) & 0x7] = rol32(t[(6 * round + 3) & 0x7] + rol32(delta, round + 3), 11);
+    
+    for(round = 0; round < LEA256_ROUNDS; ++round) {
+        uint32_t delta = DELTA[round & 0x7];
+        
+        t[(6 * round) & 0x7] = rol32(t[(6 * round) & 0x7] + rol32(delta, round), 1);
+        t[(6 * round + 1) & 0x7] = rol32(t[(6 * round + 1) & 0x7] + rol32(delta, round + 1), 3);
+        t[(6 * round + 2) & 0x7] = rol32(t[(6 * round + 2) & 0x7] + rol32(delta, round + 2), 6);
+        t[(6 * round + 3) & 0x7] = rol32(t[(6 * round + 3) & 0x7] + rol32(delta, round + 3), 11);
         t[(6 * round + 4) & 0x7] = rol32(t[(6 * round + 4) & 0x7] + rol32(delta, round + 4), 13);
         t[(6 * round + 5) & 0x7] = rol32(t[(6 * round + 5) & 0x7] + rol32(delta, round + 5), 17);
 
-		rk[0] = t[(6 * round) & 0x7];
+        rk[0] = t[(6 * round) & 0x7];
         rk[1] = t[(6 * round + 1) & 0x7];
         rk[2] = t[(6 * round + 2) & 0x7];
         rk[3] = t[(6 * round + 3) & 0x7];
         rk[4] = t[(6 * round + 4) & 0x7];
         rk[5] = t[(6 * round + 5) & 0x7];
         rk += 6;
-	}
+    }
 }
 
 void lea256_encrypt(uint8_t* out, const uint8_t* in, const uint8_t* rks)
