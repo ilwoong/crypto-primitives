3ee6717fea140bc19bac525dadcba5e9c7c1d7db
ilwoong <ilwoong.jeong@gmail.com>
2018-11-21 00:28:14 +0900

    AES NI implementation was added

---
+++ aes/aes.ni.c
@@ -0,0 +1,228 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2018 Ilwoong Jeong, https://github.com/ilwoong
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "aes.h"
+#include <wmmintrin.h>
+
+/******************************************************************************
+ * AES common functions
+ *****************************************************************************/
+static __m128i aes_keyexp1(__m128i k0, __m128i k1){
+    k1 = _mm_shuffle_epi32(k1, _MM_SHUFFLE(3, 3, 3, 3));
+
+    k0 = _mm_xor_si128(k0, _mm_slli_si128(k0, 4));
+    k0 = _mm_xor_si128(k0, _mm_slli_si128(k0, 4));
+    k0 = _mm_xor_si128(k0, _mm_slli_si128(k0, 4));
+
+    return _mm_xor_si128(k0, k1);
+}
+
+static __m128i aes_keyexp2(__m128i k0, __m128i k1){
+    k1 = _mm_shuffle_epi32(k1, _MM_SHUFFLE(2, 2, 2, 2));
+
+    k0 = _mm_xor_si128(k0, _mm_slli_si128(k0, 4));
+    k0 = _mm_xor_si128(k0, _mm_slli_si128(k0, 4));
+    k0 = _mm_xor_si128(k0, _mm_slli_si128(k0, 4));
+
+    return _mm_xor_si128(k0, k1);
+}
+
+static inline void aesni_encrypt(uint8_t *cipherText, const uint8_t *plainText, const __m128i *rks, size_t numRounds)
+{
+    int round = 0;
+    __m128i blk = _mm_loadu_si128((__m128i *) plainText);
+
+    blk = _mm_xor_si128(blk, rks[round]);
+
+    for (round = 1; round < numRounds; ++round) {
+        blk = _mm_aesenc_si128(blk, rks[round]);
+    }
+        
+    blk = _mm_aesenclast_si128(blk, rks[round]);
+
+    _mm_storeu_si128((__m128i *) cipherText, blk);
+}
+
+static inline void aesni_decrypt(uint8_t *plainText, const uint8_t *cipherText, const __m128i *rks, size_t numRounds)
+{
+    int round = numRounds;
+    __m128i blk = _mm_loadu_si128((__m128i *) cipherText);
+
+    blk = _mm_xor_si128(blk, rks[round]);
+    
+    for (round = numRounds - 1; round > 0; --round) {
+        blk = _mm_aesdec_si128(blk, _mm_aesimc_si128(rks[round]));
+    }
+
+    blk = _mm_aesdeclast_si128(blk, rks[round]);
+
+    _mm_storeu_si128((__m128i *) plainText, blk);
+}
+
+/******************************************************************************
+ * AES 128 bit key
+ *****************************************************************************/
+void aes128_keygen(uint8_t* rk, const uint8_t* mk)
+{
+    __m128i* rks = (__m128i*) rk;
+
+    rks[0]  = _mm_loadu_si128((const __m128i*) mk);
+    rks[1]  = aes_keyexp1(rks[0], _mm_aeskeygenassist_si128(rks[0], 0x01));
+    rks[2]  = aes_keyexp1(rks[1], _mm_aeskeygenassist_si128(rks[1], 0x02));
+    rks[3]  = aes_keyexp1(rks[2], _mm_aeskeygenassist_si128(rks[2], 0x04));
+    rks[4]  = aes_keyexp1(rks[3], _mm_aeskeygenassist_si128(rks[3], 0x08));
+    rks[5]  = aes_keyexp1(rks[4], _mm_aeskeygenassist_si128(rks[4], 0x10));
+    rks[6]  = aes_keyexp1(rks[5], _mm_aeskeygenassist_si128(rks[5], 0x20));
+    rks[7]  = aes_keyexp1(rks[6], _mm_aeskeygenassist_si128(rks[6], 0x40));
+    rks[8]  = aes_keyexp1(rks[7], _mm_aeskeygenassist_si128(rks[7], 0x80));
+    rks[9]  = aes_keyexp1(rks[8], _mm_aeskeygenassist_si128(rks[8], 0x1b));
+    rks[10]  = aes_keyexp1(rks[9], _mm_aeskeygenassist_si128(rks[9], 0x36));
+}
+
+void aes128_encrypt(uint8_t *cipherText, const uint8_t *plainText, const uint8_t *rks)
+{
+    aesni_encrypt(cipherText, plainText, (__m128i*) rks, AES128_ROUNDS);
+}
+
+void aes128_decrypt(uint8_t *plainText, const uint8_t *cipherText, const uint8_t *rks)
+{
+    aesni_decrypt(plainText, cipherText, (__m128i*) rks, AES128_ROUNDS);
+}
+
+/******************************************************************************
+ * AES 192 bit key
+ *****************************************************************************/
+static void aes192_keyexp(__m128i* pk1, __m128i* pk2, __m128i k2_rcon, uint32_t* out)
+{
+    __m128i k1 = *pk1;
+    __m128i k2 = *pk2;
+    k2_rcon = _mm_shuffle_epi32(k2_rcon, _MM_SHUFFLE(1, 1, 1, 1));
+    k1 = _mm_xor_si128(k1, _mm_slli_si128(k1, 4));
+    k1 = _mm_xor_si128(k1, _mm_slli_si128(k1, 4));
+    k1 = _mm_xor_si128(k1, _mm_slli_si128(k1, 4));
+    k1 = _mm_xor_si128(k1, k2_rcon);
+    
+    *pk1 = k1;
+    _mm_storeu_si128((__m128i*)out, k1);
+    
+    k2 = _mm_xor_si128(k2, _mm_slli_si128(k2, 4));
+    k2 = _mm_xor_si128(k2, _mm_shuffle_epi32(k1, _MM_SHUFFLE(3, 3, 3, 3)));
+
+    *pk2 = k2;
+    out[4] = _mm_cvtsi128_si32(k2);
+    out[5] = _mm_cvtsi128_si32(_mm_srli_si128(k2, 4));
+}
+
+static void aes192_keyexp_final(__m128i* pk1, __m128i* pk2, __m128i k2_rcon, uint32_t* out)
+{
+    __m128i k1 = *pk1;
+    __m128i k2 = *pk2;
+    k2_rcon = _mm_shuffle_epi32(k2_rcon, _MM_SHUFFLE(1, 1, 1, 1));
+    
+    k1 = _mm_xor_si128(k1, _mm_slli_si128(k1, 4));
+    k1 = _mm_xor_si128(k1, _mm_slli_si128(k1, 4));
+    k1 = _mm_xor_si128(k1, _mm_slli_si128(k1, 4));
+    k1 = _mm_xor_si128(k1, k2_rcon);
+    
+    *pk1 = k1;
+    _mm_storeu_si128((__m128i*)out, k1);
+}
+
+void aes192_keygen(uint8_t* rk, const uint8_t* mk)
+{
+    __m128i k1, k2;
+    uint32_t* rks = (uint32_t*) rk;
+    int i = 0;
+
+    for (i = 0; i < 24; ++i) {
+        rk[i] = mk[i];
+    }
+
+    k1 = _mm_loadu_si128((const __m128i*) mk);
+    k2 = _mm_loadu_si128((const __m128i*) (mk + 16));
+
+    aes192_keyexp(&k1, &k2, _mm_aeskeygenassist_si128(k2, 0x01), rks += 6);
+    aes192_keyexp(&k1, &k2, _mm_aeskeygenassist_si128(k2, 0x02), rks += 6);
+    aes192_keyexp(&k1, &k2, _mm_aeskeygenassist_si128(k2, 0x04), rks += 6);
+    aes192_keyexp(&k1, &k2, _mm_aeskeygenassist_si128(k2, 0x08), rks += 6);
+    aes192_keyexp(&k1, &k2, _mm_aeskeygenassist_si128(k2, 0x10), rks += 6);
+    aes192_keyexp(&k1, &k2, _mm_aeskeygenassist_si128(k2, 0x20), rks += 6);
+    aes192_keyexp(&k1, &k2, _mm_aeskeygenassist_si128(k2, 0x40), rks += 6);
+    aes192_keyexp_final(&k1, &k2, _mm_aeskeygenassist_si128(k2, 0x80), rks += 6);
+
+}
+
+void aes192_encrypt(uint8_t* ct, const uint8_t* pt, const uint8_t* rks)
+{
+    aesni_encrypt(ct, pt, (__m128i*) rks, AES192_ROUNDS);
+}
+
+void aes192_decrypt(uint8_t* pt, const uint8_t* ct, const uint8_t* rks)
+{
+    aesni_decrypt(pt, ct, (__m128i*) rks, AES192_ROUNDS);
+}
+
+/******************************************************************************
+ * AES 256 bit key
+ *****************************************************************************/
+void aes256_keygen(uint8_t* rk, const uint8_t* mk)
+{
+    __m128i* rks = (__m128i*) rk;
+
+    rks[0] = _mm_loadu_si128((const __m128i*) mk);
+    rks[1] = _mm_loadu_si128((const __m128i*) (mk+16));
+    
+    rks[2] = aes_keyexp1(rks[0], _mm_aeskeygenassist_si128(rks[1], 0x1));
+    rks[3] = aes_keyexp2(rks[1], _mm_aeskeygenassist_si128(rks[2], 0x0));
+
+    rks[4] = aes_keyexp1(rks[2], _mm_aeskeygenassist_si128(rks[3], 0x2));
+    rks[5] = aes_keyexp2(rks[3], _mm_aeskeygenassist_si128(rks[4], 0x0));
+
+    rks[6] = aes_keyexp1(rks[4], _mm_aeskeygenassist_si128(rks[5], 0x4));
+    rks[7] = aes_keyexp2(rks[5], _mm_aeskeygenassist_si128(rks[6], 0x0));
+
+    rks[8] = aes_keyexp1(rks[6], _mm_aeskeygenassist_si128(rks[7], 0x8));
+    rks[9] = aes_keyexp2(rks[7], _mm_aeskeygenassist_si128(rks[8], 0x0));
+
+    rks[10] = aes_keyexp1(rks[8], _mm_aeskeygenassist_si128(rks[9], 0x10));
+    rks[11] = aes_keyexp2(rks[9], _mm_aeskeygenassist_si128(rks[10], 0x0));
+
+    rks[12] = aes_keyexp1(rks[10], _mm_aeskeygenassist_si128(rks[11], 0x20));
+    rks[13] = aes_keyexp2(rks[11], _mm_aeskeygenassist_si128(rks[12], 0x0));
+    
+    rks[14] = aes_keyexp1(rks[12], _mm_aeskeygenassist_si128(rks[13], 0x40));
+}
+
+void aes256_encrypt(uint8_t* ct, const uint8_t* pt, const uint8_t* rks)
+{
+    aesni_encrypt(ct, pt, (__m128i*) rks, AES256_ROUNDS);
+}
+
+void aes256_decrypt(uint8_t* pt, const uint8_t* ct, const uint8_t* rks)
+{
+    aesni_decrypt(pt, ct, (__m128i*) rks, AES256_ROUNDS);
+}
---
+++ aes/aes_test.c
@@ -0,0 +1,136 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2018 Ilwoong Jeong, https://github.com/ilwoong
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "aes.h"
+#include <stdio.h>
+#include <string.h>
+
+static void print_hex(const char* title, const uint8_t* data, size_t count)
+{
+    printf("%s: ", title);
+    for (size_t i = 0; i < count; ++i) {
+        printf("%02x", data[i]);
+
+        if (((i+1) & 0xf) == 0) {
+            printf("\n");
+        } else if ( ((i+1) & 0x3) == 0) {
+            printf(" ");
+        }
+    }
+
+    if ( (count & 0xf) != 0) {
+        printf("\n");
+    }
+    
+}
+
+static void compare_block(const char* title, const uint8_t* pt, const uint8_t* ct, const uint8_t* enc, const uint8_t* dec)
+{
+    int out = 0;
+    if(memcmp(ct, enc, 16)) out=1;
+    if(memcmp(pt, dec, 16)) out|=2;
+
+    printf("%s\n", title);
+    print_hex("ct", enc, 16);
+    print_hex("pt", dec, 16);
+
+    if (out == 0) {
+        printf("passed\n");
+    }
+
+    if (out & 0x1) {
+        printf("encryption failed\n");
+    }
+
+    if (out & 0x2) {
+        printf("decryption failed\n");
+    }
+    printf("\n");
+}
+
+//return 0 if no error
+//1 if encryption failed
+//2 if decryption failed
+//3 if both failed
+static int aes128_self_test(void)
+{
+    uint8_t mk[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+    uint8_t pt[] = {0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34};
+    uint8_t ct[] = {0x39, 0x25, 0x84, 0x1d, 0x02, 0xdc, 0x09, 0xfb, 0xdc, 0x11, 0x85, 0x97, 0x19, 0x6a, 0x0b, 0x32};
+    
+    uint8_t enc[16] = {0};
+    uint8_t dec[16] = {0};
+
+    uint8_t rks[(AES128_ROUNDS + 1) * 16] = {0,};
+    aes128_keygen(rks, mk);
+
+    aes128_encrypt(enc, pt, rks);
+    aes128_decrypt(dec, ct, rks);
+    compare_block("AES-128", pt, ct, enc, dec);
+}
+
+static int aes192_self_test() 
+{
+    uint8_t mk[] = {0x8e, 0x73, 0xb0, 0xf7, 0xda, 0x0e, 0x64, 0x52, 0xc8, 0x10, 0xf3, 0x2b, 0x80, 0x90, 0x79, 0xe5, 0x62, 0xf8, 0xea, 0xd2, 0x52, 0x2c, 0x6b, 0x7b};
+    uint8_t pt[] = {0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a};
+    uint8_t ct[] = {0xbd, 0x33, 0x4f, 0x1d, 0x6e, 0x45, 0xf2, 0x5f, 0xf7, 0x12, 0xa2, 0x14, 0x57, 0x1f, 0xa5, 0xcc};
+    
+    uint8_t enc[16] = {0};
+    uint8_t dec[16] = {0};
+
+    uint8_t rks[(AES192_ROUNDS + 1) * 16] = {0,};
+    aes192_keygen(rks, mk);
+
+    aes192_encrypt(enc, pt, rks);
+    aes192_decrypt(dec, ct, rks);
+    compare_block("AES-192", pt, ct, enc, dec);
+}
+
+static int aes256_self_test() 
+{
+    uint8_t mk[] = {0x60, 0x3d, 0xeb, 0x10, 0x15, 0xca, 0x71, 0xbe, 0x2b, 0x73, 0xae, 0xf0, 0x85, 0x7d, 0x77, 0x81, 0x1f, 0x35, 0x2c, 0x07, 0x3b, 0x61, 0x08, 0xd7, 0x2d, 0x98, 0x10, 0xa3, 0x09, 0x14, 0xdf, 0xf4};
+    uint8_t pt[] = {0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a};
+    uint8_t ct[] = {0xf3, 0xee, 0xd1, 0xbd, 0xb5, 0xd2, 0xa0, 0x3c, 0x06, 0x4b, 0x5a, 0x7e, 0x3d, 0xb1, 0x81, 0xf8};
+    
+    uint8_t enc[16] = {0};
+    uint8_t dec[16] = {0};
+
+    uint8_t rks[(AES256_ROUNDS + 1) * 16] = {0,};
+    aes256_keygen(rks, mk);
+
+    aes256_encrypt(enc, pt, rks);
+    aes256_decrypt(dec, ct, rks);
+    compare_block("AES-256", pt, ct, enc, dec);
+}
+
+int main()
+{
+    aes128_self_test();
+    aes192_self_test();
+    aes256_self_test();
+    return 0;
+}
